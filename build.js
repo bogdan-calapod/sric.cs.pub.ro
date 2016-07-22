93f5e8b8e0d74f2c6ce672c08cdd7a257ba939d1
Bogdan Calapod [Laptop] <bogdan.marius.calapod@outlook.com>
2016-07-22 15:10:22 +0300

    Add header

--- build.js
+++ build.js
@@ -9,19 +9,26 @@ let pug = require('pug');
 let sass = require('node-sass');
 //let webpack = require('webpack');
 let fs = require('fs');
+let ncp = require('ncp').ncp;
 
 // Render pug files
+
+let data = require('./data.js');
+
 console.log('[StatGen] Rendering Pug files');
-let html = pug.renderFile('./src/index.pug');
+let html = pug.renderFile('./src/index.pug', data);
 
 console.log('[StatGen] Writing index.html file');
 fs.writeFile('./build/index.html', html);
 
 // Render SASS files
+let sassIncludePaths = require('bourbon-neat').includePaths;
+sassIncludePaths.push('./node_modules/normalize');
+
 console.log('[StatGen] Compiling SCSS files ')
 sass.render({
   file: './src/styles/index.scss',
-  includePaths: require('bourbon-neat').includePaths
+  includePaths: sassIncludePaths
 },
   (err, res) => {
     if(err)
@@ -31,5 +38,11 @@ sass.render({
     fs.writeFile('./build/css/style.css', res.css);
   }
 );
+
+// Move fonts
+ncp('./src/styles/font', './build/css/font');
+
+// Move static assets
+ncp('./src/img', './build/img');
 
 //TODO Compile JavaScript
---
+++ data.js
@@ -0,0 +1,72 @@
+let data = {
+  courses: {
+    sem1 : [
+      {
+        name: 'Network Security',
+        desc: 'Irure aliquip nostrud culpa ad exercitation proident in sint Lorem labore ipsum anim eu id aliqua eu.',
+        optional: false
+      },
+      {
+        name: 'Network service management',
+        desc: 'Excepteur cupidatat aliqua ea do veniam sint incididunt elit voluptate voluptate non commodo adipisicing consequat anim.',
+        optional: false
+      },
+      {
+        name: 'OS Development',
+        desc: 'Excepteur cupidatat aliqua ea do veniam sint incididunt elit voluptate voluptate non commodo adipisicing consequat anim.',
+        optional: false
+      },
+      {
+        name: 'Something something',
+        desc: 'Excepteur cupidatat aliqua ea do veniam sint incididunt elit voluptate voluptate non commodo adipisicing consequat anim.',
+        optional: true
+      }
+    ],
+    sem2 : [
+      {
+        name: 'Network Security',
+        desc: 'Irure aliquip nostrud culpa ad exercitation proident in sint Lorem labore ipsum anim eu id aliqua eu.',
+        optional: false
+      },
+      {
+        name: 'Network service management',
+        desc: 'Excepteur cupidatat aliqua ea do veniam sint incididunt elit voluptate voluptate non commodo adipisicing consequat anim.',
+        optional: false
+      },
+      {
+        name: 'OS Development',
+        desc: 'Excepteur cupidatat aliqua ea do veniam sint incididunt elit voluptate voluptate non commodo adipisicing consequat anim.',
+        optional: false
+      },
+      {
+        name: 'Something something',
+        desc: 'Excepteur cupidatat aliqua ea do veniam sint incididunt elit voluptate voluptate non commodo adipisicing consequat anim.',
+        optional: true
+      }
+    ],
+    sem3 : [
+      {
+        name: 'Network Security',
+        desc: 'Irure aliquip nostrud culpa ad exercitation proident in sint Lorem labore ipsum anim eu id aliqua eu.',
+        optional: false
+      },
+      {
+        name: 'Network service management',
+        desc: 'Excepteur cupidatat aliqua ea do veniam sint incididunt elit voluptate voluptate non commodo adipisicing consequat anim.',
+        optional: false
+      },
+      {
+        name: 'OS Development',
+        desc: 'Excepteur cupidatat aliqua ea do veniam sint incididunt elit voluptate voluptate non commodo adipisicing consequat anim.',
+        optional: false
+      },
+      {
+        name: 'Something something',
+        desc: 'Excepteur cupidatat aliqua ea do veniam sint incididunt elit voluptate voluptate non commodo adipisicing consequat anim.',
+        optional: true
+      }
+    ]
+  }
+};
+
+module.exports = {data};

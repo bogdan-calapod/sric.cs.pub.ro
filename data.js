d5b477de2f5e03373102d59cd05eeaa853f672f5
Bogdan Calapod [Laptop] <bogdan.marius.calapod@outlook.com>
2016-07-22 16:47:34 +0300

    Add career section

--- data.js
+++ data.js
@@ -68,7 +68,27 @@ let data = {
     ]
   },
   copy: {
-    why: 'Laboris commodo nisi tempor excepteur officia consequat sit ipsum magna ea ullamco laboris amet voluptate ullamco laborum. Sint officia minim do nostrud adipisicing nulla adipisicing Lorem incididunt exercitation id quis consequat laboris nulla. Reprehenderit non anim cillum excepteur id esse sit amet adipisicing amet incididunt magna quis. Ipsum quis ex adipisicing Lorem laborum et officia nulla minim laboris id enim. Ea proident in reprehenderit nisi aliqua esse deserunt aliqua. Occaecat minim eiusmod irure Lorem cillum tempor velit ut mollit eiusmod duis fugiat aliqua tempor qui nulla laboris.'
+    why: 'Laboris commodo nisi tempor excepteur officia consequat sit ipsum magna ea ullamco laboris amet voluptate ullamco laborum. Sint officia minim do nostrud adipisicing nulla adipisicing Lorem incididunt exercitation id quis consequat laboris nulla. Reprehenderit non anim cillum excepteur id esse sit amet adipisicing amet incididunt magna quis. Ipsum quis ex adipisicing Lorem laborum et officia nulla minim laboris id enim. Ea proident in reprehenderit nisi aliqua esse deserunt aliqua. Occaecat minim eiusmod irure Lorem cillum tempor velit ut mollit eiusmod duis fugiat aliqua tempor qui nulla laboris.',
+    career: {
+      desc: 'Velit ea non ipsum eiusmod eu duis enim officia in cillum anim. Sunt commodo fugiat proident ex irure Lorem amet excepteur consequat consectetur. Est est officia est proident proident ut nulla sit Lorem id fugiat aliquip nulla laboris id voluptate officia. Amet do esse nostrud ad et aliqua elit mollit sunt fugiat sint ex sint consequat. Do aliqua veniam enim laborum cillum officia ex reprehenderit id duis.',
+      careers: [
+        {
+          name: 'Ad adipisicing deserunt',
+          desc: 'Consequat nulla ut minim do aute dolor in eiusmod tempor in consectetur laboris eu. Est mollit eu id aute consectetur officia deserunt. Ipsum tempor qui ex laboris est non do esse est Lorem non quis ea voluptate. Reprehenderit aute cupidatat laborum eu eiusmod velit minim do. Dolor in consequat culpa occaecat ea commodo velit adipisicing deserunt laboris.',
+          image: 'course.png'
+        },
+        {
+          name: 'Deserunt',
+          desc: 'Consequat nulla ut minim do aute dolor in eiusmod tempor in consectetur laboris eu. Est mollit eu id aute consectetur officia deserunt. Ipsum tempor qui ex laboris est non do esse est Lorem non quis ea voluptate. Reprehenderit aute cupidatat laborum eu eiusmod velit minim do. Dolor in consequat culpa occaecat ea commodo velit adipisicing deserunt laboris.',
+          image: 'course.png'
+        },
+        {
+          name: 'Aipisicing',
+          desc: 'Consequat nulla ut minim do aute dolor in eiusmod tempor in consectetur laboris eu. Est mollit eu id aute consectetur officia deserunt. Ipsum tempor qui ex laboris est non do esse est Lorem non quis ea voluptate. Reprehenderit aute cupidatat laborum eu eiusmod velit minim do. Dolor in consequat culpa occaecat ea commodo velit adipisicing deserunt laboris.',
+          image: 'course.png'
+        }
+      ]
+    }
   }
 };
 

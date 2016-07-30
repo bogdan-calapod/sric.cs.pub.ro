db645df9d6099ff489e8052facdcd3062ae243d8
Bogdan Calapod [Laptop] <bogdan.marius.calapod@outlook.com>
2016-07-30 17:40:49 +0300

    Update content

--- data.js
+++ data.js
@@ -5,7 +5,9 @@ let data = {
     why: [
       `Security professionals are increasingly called upon to migrate from the boundaries and gates of information systems to the very core of  digital infrastructure design and management. Information systems are permeating all fields of daily lives, from the intimate zones of the household to the corporate world. Appliances become interconnected in the Internet of Things; people work, chat, and observe reality through mobile connections; entire continents of business migrate to the cloud. Meanwhile, the stakes of malicious attacks become higher, and vulnerabilities increase, in the mismatch of old and new technologies and interconnected platforms.`,
 
-      `Join SRIC to gain indepth knowledge, hands-on mastery and broader perspectives on a variety of technologies of cybersecurity. Make your voice heard in the SRIC community of alumni, professors, students and security specialists.`
+      `Join SRIC to gain indepth knowledge, hands-on mastery and broader perspectives on a variety of technologies of cybersecurity. Make your voice heard in the SRIC community of alumni, professors, students and security specialists.`,
+
+      `If you enjoy making computing systems work, hardening and securing system components and continuously improving IT infrastructures, you are the ideal candidate for the SRIC masters program.`
     ],
     career: {
       careers: [ {

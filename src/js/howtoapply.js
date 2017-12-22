90f02fd5f240817808118faad186fb4f1bdd4e5a
bogdan.marius.calapod <bogdan.marius.calapod@gmail.com>
2017-12-22 19:01:57 +0200

    Initial Gatsby commit

--- src/js/howtoapply.js
+++
@@ -1,9 +0,0 @@
-let applyButton = document.querySelector('#apply'),
-    howtoSection = document.querySelector('section.howtoapply');
-
-applyButton.addEventListener(
-  'click',
-  e => {
-    howtoSection.classList.toggle('active');
-  }
-)
